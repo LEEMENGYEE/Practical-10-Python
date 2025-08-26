f2cf291a36991e09c7ffb4d8d785a7171e86c388
LEEMENGYEE <leemengyee3996@gmail.com>
2025-08-26 19:33:43 +0800

    P10

---
+++ Practical-10(Python)/Q1/Rectangle.py
@@ -0,0 +1,30 @@
+from GeometricObject import *
+
+class Rectangle(GeometricObject):
+    def __init__(self, colour="white", filled=False, width=1.0, height=1.0):
+        super().__init__(colour, filled)
+        self.width = width
+        self.height = height
+
+    def get_width(self):
+        return self.width
+
+    def set_width(self, width):
+        self.width = width
+
+    def get_height(self):
+        return self.height
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        return "{}, width: {:.2f}, height: {:.2f}".format(
+            super().__str__(), self.width, self.height
+        )
